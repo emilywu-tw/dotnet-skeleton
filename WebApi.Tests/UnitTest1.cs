6512f3c38eb36236ce6ace569998adeeea8307e9
Corey Grad <cgrad01@gmail.com>
2020-01-09 12:08:56 -0500

    creates test project and solution file, Corey, Emily

---
+++ WebApi.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace WebApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+            Assert.False(false);
+        }
+    }
+}
