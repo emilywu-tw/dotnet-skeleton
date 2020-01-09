108d0717be06c1b18700b95b5604ada581ad3e4a
Corey Grad <cgrad01@gmail.com>
2020-01-09 13:52:36 -0500

    Add unit test for endpoint | Corey, Emily

--- WebApi.Tests/UnitTest1.cs
+++ WebApi.Tests/UnitTest1.cs
@@ -1,15 +1,31 @@
 using System;
 using Xunit;
+using TestWebApi;
+using TestWebApi.Controllers;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace WebApi.Tests
 {
     public class UnitTest1
     {
+        private readonly ILogger<WeatherForecastController> _logger;
+        
         [Fact]
         public void Test1()
         {
             Assert.True(true);
             Assert.False(false);
+        }
+
+        [Fact]
+        public void ForecastTest()
+        {
+        WeatherForecastController controller = new WeatherForecastController(_logger);
+    
+        var okResult = controller.Get();
+ 
+        Assert.IsType<WeatherForecast[]>(okResult);
         }
     }
 }
